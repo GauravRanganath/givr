e2c52bbf199e3c29205cb1e5bf0ed460fbcd78bc
Tommy <tommyliu9@gmail.com>
2019-09-15 05:00:38 -0400

    ohe sec

--- charity_hat/lib/firstpage.dart
+++ charity_hat/lib/firstpage.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:localstorage/localstorage.dart';
+import 'dart:convert';
+
+const baseUrl = "http://localhost:3000";
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key key}) : super(key: key);
-
+  
+  void fetchExpenses() async {
+     final LocalStorage storage = new LocalStorage('some_key');
+      String user = storage.getItem("username");
+      const route = baseUrl + "/expenses/redcross";
+        
+      Response res = await get(route);
+      final body = json.decode(res.body);
+      
+      
+      debugPrint(body);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +29,9 @@ class FirstPage extends StatelessWidget {
         return ListTile(
           title: Text('Lorem Ipsum'),
           subtitle: Text('$index'),
+          onTap: () async {
+            fetchExpenses();
+          },
         );
       }),
     );
