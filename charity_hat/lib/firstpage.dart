50835523473cbaff87ed054fa5580364346afea9
GauravRanganath <46269824+GauravRanganath@users.noreply.github.com>
2019-09-15 06:19:24 -0400

    frontend update

--- charity_hat/lib/firstpage.dart
+++ charity_hat/lib/firstpage.dart
@@ -21,19 +21,132 @@ class FirstPage extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
+    List<Widget> alist = new List<Widget>();
+
+    for (int i = 0; i < 3;i++){
+      alist.add(
+          Container(
+            color: Colors.white10,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              color: Colors.grey[300],
+              margin: EdgeInsets.all(15),
+              child: Container(
+                color: Colors.grey[700],
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+
+                    Padding(padding: EdgeInsets.all(10)),
+
+                    Text(
+                      'EXPENSES',
+                      style: TextStyle(
+                        fontFamily: 'Lato',
+                        color: Colors.greenAccent,
+                        fontSize: 26,
+                        letterSpacing: 5,
+                      ),
+                    ),
+
+                    Padding(padding: EdgeInsets.all(10)),
+
+                    Text(
+                        '\$45',
+                        style: TextStyle(
+                      fontFamily: 'Lato',
+                      color: Colors.white,
+                      fontSize: 40,
+                          letterSpacing: 5,
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("First Screen"),
-      ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Lorem Ipsum'),
-          subtitle: Text('$index'),
-          onTap: () async {
-            fetchExpenses();
-          },
-        );
-      }),
+      body:
+          Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                CircleImage(),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                Text(
+                  'GAURAV RANGANATH',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'Lato',
+                    letterSpacing: 2,
+                    color: Colors.black,
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(5),
+                ),
+
+                Text(
+                  'CHARITIES: RED CROSS AND DISCOVERY HOUSE',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'Lato',
+                    letterSpacing: 2,
+                    color: Colors.black,
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(2),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20.0),
+                  height: 200.0,
+                  child:
+                  Center(
+                    child:
+                    ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: alist,
+                    ),
+                  ),),
+              ],
+            ),
+          ),
+
+
     );
   }
+}
+
+class CircleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _size = 200.0;
+
+    return Center(
+      child: new Container(
+          width: _size,
+          height: _size,
+          decoration: new BoxDecoration(
+              border: Border.all(width: 5.0, color: Colors.grey[350]),
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                  fit: BoxFit.cover,
+                  image: new NetworkImage(
+                      "https://scontent.fyyz1-1.fna.fbcdn.net/v/t1.0-9/35305554_930165850481980_5000180563563249664_n.jpg?_nc_cat=104&_nc_oc=AQmQ7wBPSMAsTbMUPc60VM47oIaed29NX3hZizDTUeuMitInhv5zDFxSLLiJgKKEgDM&_nc_ht=scontent.fyyz1-1.fna&oh=cd82af78deeeeae65e063115a8b38822&oe=5E0D4153")
+              )
+          )
+      ),
+    );
+  }
+
 }
